71c22f78179fa4e16f91e019c5d05ce542cf72db
R Looper <looper@example.com>
2017-09-11 19:11:01 -0700

    Add entries for /boot and /mnt/data into /etc/fstab.

--- pi3br/buildroot/board/raspberrypi/post-build.sh
+++ pi3br/buildroot/board/raspberrypi/post-build.sh
@@ -9,3 +9,16 @@ if [ -e ${TARGET_DIR}/etc/inittab ]; then
 	sed -i '/GENERIC_SERIAL/a\
 tty1::respawn:/sbin/getty -L  tty1 0 vt100 # HDMI console' ${TARGET_DIR}/etc/inittab
 fi
+
+TARGETROOT=$1
+BR_ROOT=$PWD
+
+# mount points
+mkdir -p $TARGETROOT/boot
+mkdir -p $TARGETROOT/mnt/data
+
+# fstab
+install -T -m 0644 $BR_ROOT/system/skeleton/etc/fstab $TARGETROOT/etc/fstab
+echo '/dev/mmcblk0p1 /boot auto defaults 0 0' >> $TARGETROOT/etc/fstab
+echo '/dev/mmcblk0p3 /mnt/data auto defaults 0 0' >> $TARGETROOT/etc/fstab
+
